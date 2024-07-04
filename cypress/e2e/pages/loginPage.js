12d0cfc2cce6d79f947191072a86854a21b59f1d
Miguel Millan <millananayamiguel@gmail.com>
2024-07-04 23:24:26 +0200

    repor6

--- cypress/e2e/pages/loginPage.js
+++ cypress/e2e/pages/loginPage.js
@@ -1,12 +1,15 @@
+const myAccountType ='.dropdown';
+const registreSelect ='.dropdown-menu > :nth-child(1) > a';
+
 export class LoginPage {
 
     typeMyAccount(){
         //cy.get('.dropdown',{timeout:8000}).should('be.visible').first().click()
-        cy.get('.dropdown',{timeout:8000}).should('be.visible').eq(0).click()
+        cy.get(myAccountType,{timeout:8000}).should('be.visible').eq(0).click()
 
     }
     selectRegistre(){
-        cy.get('.dropdown-menu > :nth-child(1) > a').click()
+        cy.get(registreSelect).should('be.visible').click()
 
     }
 
